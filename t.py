7906af0b8b552303121da68750a0bd97ae56447f
sainquake <sainquake@gmail.com>
2023-05-21 00:38:15 +0300

    123

--- t.py
+++ t.py
@@ -1,8 +1,19 @@
 # pip install GitPython
 
+import git
 from git import Repo
 
 local_repo = Repo()
 
 for refs in local_repo.heads:
     print(refs.name)
+
+local_repo.git.checkout('boards', 'existed-boards.md')
+
+f = open('existed-boards.md', "r")
+print(f.read())
+f.close()
+
+import os
+
+os.remove('existed-boards.md')
